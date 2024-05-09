b9263d115cd899371da208979fa5265764ac4e79
Khaipamh <160567832+Khaipamh@users.noreply.github.com>
2024-05-09 08:50:05 +0700

    Add files via upload

--- bai1.cpp
+++ bai1.cpp
@@ -1,35 +1,54 @@
 #include <iostream>
 using namespace std;
-
-class Sinhvien{
-    private:
-    string MSV;
-    int diemToan;
-
+class Hinh{
     public:
-    void setMSV(string x){
-        MSV = x;
+    virtual float dt(){
+        return 0;
     }
-    string getMsv(){
-        return MSV;
+};
+class HinhChuNhat: public Hinh{
+    float chieuDai, chieuRong;
+    public:
+    HinhChuNhat(float x, float y){
+        chieuDai = x;
+        chieuRong = y;
     }
-    void setDiemToann(int x){
-        diemToan = x;
+    float dt(){
+        return chieuDai*chieuRong;
     }
-    int getDiemToan(){
-    return diemToan;
+};
+class HinhTron: public Hinh{
+    float banKinh;
+    const float pi = 3.14159;
+    public:
+    HinhTron(float x){
+        banKinh = x;
+    }
+    float dt(){
+        return pi*banKinh*banKinh;
+    }
+};
+class TamGiacVuong: public Hinh{
+    float canh1, canh2;
+    public:
+    TamGiacVuong(float x,float y){
+        canh1 = x;
+        canh2 = y;
+    }
+    float dt(){
+        return canh1*canh2*1/2;
     }
 };
 int main(){
-    string msv;
-    int diem;
-    Sinhvien sv;
-    cout << "Nhap Ho va Ten: ";
-    cin >> msv;
-    cout << "Nhap diem: ";
-    cin >> diem;
-    sv.setMSV("Nguyen Van A");
-    sv.setDiemToann(9);
-    cout << "Diem toan sinh vien "<< sv.setMSV() <<"la: " << sv.setDiemToann();
+    Hinh* hinh;
+    HinhChuNhat hinhChuNhat(13,6);
+    HinhTron hinhTron(9);
+    TamGiacVuong tamGiacVuong(3,1);
+    hinh = &hinhChuNhat;
+    cout << "Dien tich hinh Chu Nhat la: "<<hinh ->dt()<<endl;
+    hinh = &hinhTron;
+    cout << "Dien tich hinh Tron la: "<<hinh ->dt()<<endl;
+    hinh = &tamGiacVuong;
+    cout << "Dien tich hinh Tam Giac Vuong la: "<<hinh ->dt()<<endl;
     return 0;
 }
